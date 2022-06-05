421746e6ee6691686d1b398d5942c5a09a13937a
Vendetta <34066247+vendettadelsangue@users.noreply.github.com>
2022-06-05 19:40:22 +0900

    update web Issue

--- AutoAnnounceDiscordBot/index.js
+++ AutoAnnounceDiscordBot/index.js
@@ -23,11 +23,8 @@ client.once('ready', () => {
                     description: "[https://CistusF.github.io/Jj_Python_StudyRoom/](https://CistusF.github.io/Jj_Python_StudyRoom/)",
                     color: "BLURPLE",
                     fields: [{
-                        name: "KR",
-                        value: "위 링크를 누르면 파일이 다운됩니다!\n압축을 푼 뒤 Quizzes에 있는 문제들을 풀어서 Answers.txt에 저장후 저에게 보내주세요!\n시험 시간은 1시간 30분이며 못푼 문제는 그냥 두고 보내시면 됩니다!\n파이썬 파일을 직접 실행해 보시는 것에 대해서는 부정행위이오니 메모장 또는 노트에서 풀어봐 주시길 바라오며\n2차 공유는 금하고 있습니다!"
-                    }, {
-                        name: "EN",
-                        value: "Click the link above to download the file!\nAfter unpacking, unpack the questions in Quizzes, save it to Answers.txt and send it to me!\nThe duration of the exam is 1 hour and 30 minutes, and you can just leave the questions you can't solve!\nTrying to run the Python file directly is cheating, so please try it out in notepad or notebook.\nSecondary sharing is prohibited!"
+                        name: "설명",
+                        value: "위 링크를 클릭하여 문제를 볼 수 있습니다!\n문제에 대한 답안은 메모장과 같은곳에 옮기거나\n저에게 개인 메세지로 보내주시면 됩니다!\n\n일부러 어렵게 내보았습니다!\n화이팅입니다!"
                     }]
                 });
                 member.send({ embeds: [embed] });
