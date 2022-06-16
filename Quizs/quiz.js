84e723f4bf6861c5e505e8713eedc2acc056b2e9
Vendetta <34066247+vendettadelsangue@users.noreply.github.com>
2022-06-16 16:13:39 +0900

    Update Quiz

--- Quizs/quiz.js
+++ Quizs/quiz.js
@@ -29,7 +29,6 @@ var table = document.querySelector("#Table");
         "quiz": string
     };
 */
-
 const quizData = [
     {
         "type": 1,
@@ -67,7 +66,7 @@ const quizData = [
     }, {
         "type": 1,
         "quiz": "아래 재귀함수의 결과값으로 옳은것을 고르시오.",
-        "code": "bus = [False, False, True, False, True]\n\ndef Loop(n):\n\tif bus[n] == True\n\t\treturn n\n\treturn Loop(n + 1)\nLoop(0)",
+        "code": "bus = [False, False, True, False, True]\n\ndef Loop(n):\n\tif bus[n] == True:\n\t\treturn n\n\treturn Loop(n + 1)\nprint(Loop(0))",
         "list": [
             1,
             2,
@@ -108,6 +107,66 @@ const quizData = [
             "정수형",
             "튜플"
         ]
+    }, {
+        "type": 3,
+        "quiz": "1에서 100사이의 난수를 출력하게\n아래 코드를 완성하시오.",
+        "code": "from random import *"
+    }, {
+        "type": 2,
+        "quiz": "다음중 math 라이브러리에 포함되어 있지 않은 함수는?",
+        "list": [
+            "floor",
+            "choice",
+            "ceil",
+            "sqrt"
+        ]
+    }, {
+        "type": 4,
+        "quiz": "math 라이브러리에서 pi상수는 어떤 데이터를 반환하는가?"
+    }, {
+        "type": 2,
+        "quiz": "아래 코드들 중 키워드 함수를 고르시오.",
+        "list": [
+            "def Code(*c):\n\treturn c",
+            "def Code():\n\tx = 10\n\treturn x",
+            "def Code(x=10):\n\treturn x",
+            "def Code():\n\tglobal x\n\treturn x"
+        ]
+    }, {
+        "type": 3,
+        "quiz": "아래 코드의 결과값을 적으시오.",
+        "code": "Tup = (9, 5, 10, 51)\nprint(Tup[1:])"
+    }, {
+        "type": 3,
+        "quiz": "아래 코드가 올바르게 작동되기 위해 (x)에 들어갈 값을 적으시오.",
+        "code": "List = [\"튤립\", \"장미\", \"라벤더\"]\n\nfor i in (x):\n\tprint(i)\n\n출력값은 아래와 같다.\n\n튤립\n장미\n라벤더"
+    }, {
+        "type": 3,
+        "quiz": "아래 코드에서 변수 String의 값이 \'Hello, Python\' 이 되도록 코드를 완성하시오.",
+        "code": "String = \" Hello, Python\""
+    }, {
+        "type": 2,
+        "quiz": "다음 코드들 중 수식을 반환하는 방법으로 옳은것을 고르시오.",
+        "list": [
+            "global 수식",
+            "return 수식",
+            "pass 수식",
+            "continue 수식"
+        ]
+    }, {
+        "type": 1,
+        "quiz": "아래 코드를 실행하였을 경우 출력값으로 올바른것을 고르시오.",
+        "code": "List = [[4, 60 1, 5, 6]]\nList[0] = 1\nprint(List[0])",
+        "list": [
+            "4",
+            "60",
+            "1",
+            "5"
+        ]
+    }, {
+        "type": 3,
+        "quiz": "아래 코드의 결과값을 적으시오.",
+        "code": "sum([5, 7, 10, 60])"
     }
 ];
 
